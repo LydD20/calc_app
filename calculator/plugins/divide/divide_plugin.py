fe4346bf0a5f79f7bf38c205f87085086be1f323
Lydia Daids <lydiadaids7@gmaill.com>
2024-10-10 23:02:19 -0400

    added divide plugin

--- calculator/plugins/divide/divide_plugin.py
+++ calculator/plugins/divide/divide_plugin.py
@@ -1,4 +1,4 @@
-'''divide command Imports Command from calculator.commands.command'''
+'''Divide Command'''
 from calculator.commands.command import Command
 
 class DivideCommand(Command):
@@ -10,6 +10,3 @@ class DivideCommand(Command):
         if self.b == 0:
             raise ZeroDivisionError("An error occurred: Cannot divide by zero")
         return self.a / self.b
-
-# def register():
-#     return DivideCommand
